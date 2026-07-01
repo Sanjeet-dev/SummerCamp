ebef37d6f459e5872f70dd5dd9d76955c9b8a83d
sanjeet rai <raisanjeet8896@gmail.com>
2026-07-02 02:31:14 +0530

    minor changes and more code

--- practice/Minimum.java
+++ practice/Minimum.java
@@ -1,12 +1,17 @@
 package practice;
 class Minimum {
     static int min(int [] arr){
-        int min = arr[0];
-        for(int i = 1; i< arr.length; i++){
-            if(arr[i] < min){
-                min = arr[i];
-            }
+        if (arr.length == 0 || arr == null){
+            return -1;
         }
+        
+            int min = arr[0];
+            for(int i = 1; i< arr.length; i++){
+                if(arr[i] < min){
+                    min = arr[i];
+            
+        }
+    }
         return min;
     }
     public static void main(String[] args){
